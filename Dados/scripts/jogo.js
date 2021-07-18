130b695d946746a516fd6bf6eb2e24ec239b922e
Dilson Campêlo Rodrigues de Oliveira Neto <dilsono316@gmail.com>
2021-07-18 12:20:15 -0300

    Versão 2
    Mudando os dados

--- Dados/scripts/jogo.js
+++ Dados/scripts/jogo.js
@@ -2,5 +2,31 @@
  * Jogo de dados
  * @autor Dilson Campêlo
  */
+function randomNumber(){
+    var dado = document.getElementById('face')
+    var numeros = Math.floor(Math.random()*6+1)
+    switch (numeros){
+        case 1: 
+            dado.src = 'imagens/face1.png'
+            break
+        case 2:
+            dado.src = 'imagens/face2.png'
+            break
+        case 3: 
+            dado.src = 'imagens/face3.png'
+            break
+        case 4:
+            dado.src = 'imagens/face4.png'
+            break
+        case 5:
+            dado.src = 'imagens/face5.png'
+            break
+        case 6:
+            dado.src = 'imagens/face6.png'
+            break
+        default:
+            break;
+    }
+}
 
 
